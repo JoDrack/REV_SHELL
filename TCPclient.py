398a4d5548c60a73f687d614a983ab46c5c7435b
JoDrack <buingojoseph24@gmail.com>
2021-03-21 03:55:59 -0700

    Update of the client side codes

--- TCPclient.py
+++ TCPclient.py
@@ -1,3 +1,23 @@
-a = {1:'africa',2:'America'}
-for i,c in a.items():
-    print(f'{i}  :  {c}')  
+import socket
+from rich import print
+
+HOST = socket.gethostbyname(socket.gethostname())
+PORT = 1234
+ADDRESS = (HOST, PORT)
+HOST_NAME = socket.gethostname()
+
+try:
+    client = socket.socket()
+    client.connect(ADDRESS)
+    print('Connected')
+
+    while True:
+        # sending the HOST_NAME to the server
+        client.send(HOST_NAME.encode("utf-8"))
+    # print('[bold green]HostName sent successfully')
+        data = client.recv(1024).decode()
+        print(data)
+
+except ConnectionResetError as cnct_err:
+    print(f'Connection error -> {cnct_err}')
+    raise SystemExit
